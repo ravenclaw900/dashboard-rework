d3a8de9d19fd3faa0626a13ad4c8e4af7c2a54a2
ravenclaw900 <50060110+ravenclaw900@users.noreply.github.com>
2023-08-16 15:25:54 -0500

    feat(backend): add frontend integration with `vite_embed_axum`

--- src/router/routers.rs
+++ src/router/routers.rs
@@ -1,6 +1,19 @@
 use super::api;
 use crate::sysdata;
 use axum::{routing::get, Router};
+use vite_embed_axum::vite_embed::{self, ViteEmbed};
+use vite_embed_axum::vite_router;
+
+#[cfg(not(feature = "dev"))]
+const FRONTEND: ViteEmbed = vite_embed_axum::vite_embed::generate_vite_prod!(
+    "$CARGO_MANIFEST_DIR/frontend/dist/manifest.json"
+);
+
+#[cfg(feature = "dev")]
+const FRONTEND: ViteEmbed = vite_embed_axum::vite_embed::generate_vite_dev!(
+    "$CARGO_MANIFEST_DIR/frontend/index.html",
+    "src/index.tsx"
+);
 
 fn api_router() -> Router {
     let tx = sysdata::spawn_system_task();
@@ -8,5 +21,7 @@ fn api_router() -> Router {
 }
 
 pub fn router() -> Router {
-    Router::new().nest("/api", api_router())
+    let asset_router = vite_router(FRONTEND);
+
+    Router::new().nest("/api", api_router()).merge(asset_router)
 }
