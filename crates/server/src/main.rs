972b1a1e983ef7de3a3e107f484a46eda7a0ecac
ravenclaw900 <50060110+ravenclaw900@users.noreply.github.com>
2023-11-20 13:42:08 -0600

    feat(server): add TLS support using `flexible-hyper-server-tls`
    Also add about 1MB to the final compiled binary, nearly doubling the size

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,4 +1,6 @@
+use flexible_hyper_server_tls::{tlsconfig, HyperHttpOrHttpsAcceptor};
 use std::net::{Ipv6Addr, SocketAddr};
+use tokio::net::TcpListener;
 
 use config::CONFIG;
 
@@ -9,10 +11,30 @@ mod static_files;
 async fn main() {
     // Using unspecified IPv6 addr will bind to 0.0.0.0 on both v4 and v6
     let addr = SocketAddr::from((Ipv6Addr::UNSPECIFIED, CONFIG.port));
+    let listener = TcpListener::bind(addr)
+        .await
+        .expect("failed to bind to port");
 
     let router = routers::router();
 
-    axum::Server::bind(&addr)
+    let acceptor = if CONFIG.tls.enable_tls {
+        let tls_acceptor = tlsconfig::get_tlsacceptor_from_files(
+            &CONFIG.tls.cert_path,
+            &CONFIG.tls.key_path,
+            tlsconfig::HttpProtocol::Http1,
+        )
+        .expect("failed to read TLS files");
+
+        HyperHttpOrHttpsAcceptor::new_https(
+            listener,
+            tls_acceptor,
+            std::time::Duration::from_secs(10),
+        )
+    } else {
+        HyperHttpOrHttpsAcceptor::new_http(listener)
+    };
+
+    axum::Server::builder(acceptor)
         .serve(router.into_make_service())
         .await
         .expect("failed to start server");
