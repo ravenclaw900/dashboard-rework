9c0473f93ee90743a4cdad5233696eee2acd105c
ravenclaw900 <50060110+ravenclaw900@users.noreply.github.com>
2023-12-27 19:50:37 -0600

    refactor(auth): replace `bitcode` serialization with manual encoding of bytes

--- crates/auth/src/lib.rs
+++ crates/auth/src/lib.rs
@@ -2,15 +2,7 @@ use config::CONFIG;
 use ct_codecs::{Base64UrlSafeNoPadding, Decoder, Encoder, Hex};
 use ed25519_compact::{Noise, PublicKey, SecretKey, Signature};
 use ring::digest::{digest, SHA512};
-use serde::{Deserialize, Serialize};
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
-
-#[derive(Serialize, Deserialize)]
-struct Claims {
-    iss: String,
-    exp: u64,
-    nbf: u64,
-}
 
 #[must_use]
 pub fn test_password(pass: &str) -> bool {
@@ -35,16 +27,13 @@ pub fn create_token() -> String {
     let current_timestamp = current_time.duration_since(UNIX_EPOCH).unwrap().as_secs();
     let expiry_timestamp = expiry_time.duration_since(UNIX_EPOCH).unwrap().as_secs();
 
-    let claims = Claims {
-        iss: "DietPi Dashboard".to_string(),
-        exp: expiry_timestamp,
-        nbf: current_timestamp,
-    };
-    let serialized_claims = bitcode::serialize(&claims).expect("failed to serialize token");
+    let mut claims = [0_u8; 16];
+    claims[0..8].copy_from_slice(&current_timestamp.to_be_bytes());
+    claims[8..16].copy_from_slice(&expiry_timestamp.to_be_bytes());
 
-    let sig = key.sign(&serialized_claims, Some(Noise::generate()));
+    let sig = key.sign(claims, Some(Noise::generate()));
 
-    let encoded_claims = Base64UrlSafeNoPadding::encode_to_string(&serialized_claims).unwrap();
+    let encoded_claims = Base64UrlSafeNoPadding::encode_to_string(claims).unwrap();
     let encoded_sig = Base64UrlSafeNoPadding::encode_to_string(sig).unwrap();
 
     format!("dpdashboard.{encoded_claims}.{encoded_sig}")
@@ -65,7 +54,7 @@ pub fn verify_token(token: &str) -> bool {
         return false;
     }
 
-    let Ok(serialized_claims) = Base64UrlSafeNoPadding::decode_to_vec(encoded_claims, None) else {
+    let Ok(claims) = Base64UrlSafeNoPadding::decode_to_vec(encoded_claims, None) else {
         return false;
     };
     let Ok(sig) = Base64UrlSafeNoPadding::decode_to_vec(encoded_sig, None) else {
@@ -76,24 +65,33 @@ pub fn verify_token(token: &str) -> bool {
         return false;
     };
 
-    if key.verify(&serialized_claims, &sig).is_err() {
+    if key.verify(&claims, &sig).is_err() {
         return false;
     }
 
-    let claims: Claims = bitcode::deserialize(&serialized_claims).unwrap();
-
-    if claims.iss != "DietPi Dashboard" {
+    // Unwrap is fine here because, assuming it exists, we know the slice is 8 bytes long
+    let Some(claims_nbf) = claims
+        .get(0..8)
+        .map(|x| u64::from_be_bytes(x.try_into().unwrap()))
+    else {
         return false;
-    }
+    };
+
+    let Some(claims_exp) = claims
+        .get(8..16)
+        .map(|x| u64::from_be_bytes(x.try_into().unwrap()))
+    else {
+        return false;
+    };
 
     let current_time = SystemTime::now();
-    let nbf_time = UNIX_EPOCH + Duration::from_secs(claims.nbf);
+    let nbf_time = UNIX_EPOCH + Duration::from_secs(claims_nbf);
 
     if current_time < nbf_time {
         return false;
     }
 
-    let expiry_time = UNIX_EPOCH + Duration::from_secs(claims.exp);
+    let expiry_time = UNIX_EPOCH + Duration::from_secs(claims_exp);
     if current_time > expiry_time {
         return false;
     }
